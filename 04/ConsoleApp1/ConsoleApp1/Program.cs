5a0a9b9f8c2f09ced604b0ea7c45ac5a76ca346d
Nitish <nitish.webgentle@gmail.com>
2023-01-14 22:15:28 +0530

    Video 4. Difference between Console.Write() & Console.WriteLine();

---
+++ 04/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1+1);
+            Console.WriteLine("1+1");
+
+            Console.WriteLine("Second");
+            Console.Write("Third");
+        }
+    }
+}
