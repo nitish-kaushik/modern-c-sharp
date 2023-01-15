8dd27e0b461be93b8cbd9017df46eb54f9c4be7f
Nitish <nitish.webgentle@gmail.com>
2023-01-15 22:53:10 +0530

    Video 05: More concepts of console class

---
+++ 05/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Console.CapsLock);
+
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+
+            //Console.WriteLine(Console.BackgroundColor);
+            //Console.WriteLine(Console.ForegroundColor);
+
+            //Console.ResetColor();
+
+            //Console.WindowWidth = 5;
+            //Console.WindowHeight = 5;
+
+            Console.WriteLine("Hello My name is Nitish Kaushik");
+            Console.WriteLine("I like to teach the programming");
+
+            Console.Clear();
+
+        }
+    }
+}
