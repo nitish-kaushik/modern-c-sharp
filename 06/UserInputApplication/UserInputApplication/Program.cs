debc96f857500609f38776fe4aa71aa59a463594
Nitish <nitish.webgentle@gmail.com>
2023-01-23 22:28:50 +0530

    Working with Console.ReadLine() method

---
+++ 06/UserInputApplication/UserInputApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UserInputApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there, What is your name");
+
+            var input = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {input}.");
+
+            Console.WriteLine("Hello there, What is college name");
+
+            input = Console.ReadLine();
+
+            Console.WriteLine($"Your college name is {input}.");
+        }
+    }
+}
