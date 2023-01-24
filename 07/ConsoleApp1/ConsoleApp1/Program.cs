f3fdfa07242c89c840f628a297662a0b4e755d66
Nitish <nitish.webgentle@gmail.com>
2023-01-24 23:03:08 +0530

    Console.read and console.readkey

---
+++ 07/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key");
+
+            //int userInput;
+
+            //do
+            //{
+            //    userInput = Console.Read();
+            //    Console.WriteLine(Convert.ToChar(userInput));
+            //} while (userInput != 65);
+
+            var userInput = Console.ReadKey();
+
+            Console.WriteLine(userInput.KeyChar);
+          
+        }
+    }
+}
