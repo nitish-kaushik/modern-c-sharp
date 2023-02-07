37cf4bfb6ebf316c5d06dc8aa9c57997fcc61e30
Nitish <nitish.webgentle@gmail.com>
2023-02-07 15:48:20 +0530

    bool data type

---
+++ 10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    using nitish = System.Boolean;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            nitish myBool = false;
+            myBool = true;
+
+            Console.WriteLine(myBool);
+
+        }
+    }
+}
