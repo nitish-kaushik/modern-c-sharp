00dd0ee8542f8923bf050299c7630bdbfc1b387d
Nitish <nitish.webgentle@gmail.com>
2023-02-08 11:59:47 +0530

    int type

---
+++ 11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    using myCustomInt = System.Int32;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myCustomInt a = 12;
+            bool result = int.IsOddInteger(a);
+
+            Console.WriteLine(result);
+
+
+            //int first = 2147483647;
+            //int second = -2147483648;
+            //int a = 1;
+
+            //int third = a + 1;
+
+            //Console.WriteLine(third);
+
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+
+
+            //Console.WriteLine(sizeof(bool));
+
+            // 1, 2, 123, 
+
+
+            // int firstNumber, a, b = 5, c = 10;
+            //int a;
+            //int b = 5;
+            //System.Int32 secondNumber;
+
+            //firstNumber = 12345;
+            //secondNumber = 90;
+
+            //int first = 10;
+            //int second = 20;
+            //int third = first *  second;
+
+            //int myNum = -100000;
+            // Console.WriteLine(third);
+        }
+    }
+}
