5735a59b58685fe5994aae77a43800ecb8a2a898
Nitish <nitish.webgentle@gmail.com>
2023-03-26 21:39:16 +0530

    video 12

---
+++ 12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Size of float {sizeof(float)}");
+            Console.WriteLine($"Max value of float {float.MaxValue}");
+            Console.WriteLine($"Min value of float {float.MinValue}\n");
+
+            Console.WriteLine($"Size of double {sizeof(double)}");
+            Console.WriteLine($"Max value of double {double.MaxValue}");
+            Console.WriteLine($"Min value of double {double.MinValue}\n");
+
+            Console.WriteLine($"Size of decimal {sizeof(decimal)}");
+            Console.WriteLine($"Max value of decimal {decimal.MaxValue}");
+            Console.WriteLine($"Min value of decimal {decimal.MinValue}\n");
+           
+
+            Console.WriteLine($"Size of sbyte {sizeof(sbyte)}");
+            Console.WriteLine($"Max value of sbyte {sbyte.MaxValue}");
+            Console.WriteLine($"Min value of sbyte {sbyte.MinValue}\n");
+
+            Console.WriteLine($"Size of byte {sizeof(byte)}");
+            Console.WriteLine($"Max value of byte {byte.MaxValue}");
+            Console.WriteLine($"Min value of byte {byte.MinValue}\n");
+
+            Console.WriteLine($"Size of short {sizeof(short)}");
+            Console.WriteLine($"Max value of short {short.MaxValue}");
+            Console.WriteLine($"Min value of short {short.MinValue}\n");
+
+            Console.WriteLine($"Size of ushort {sizeof(ushort)}");
+            Console.WriteLine($"Max value of ushort {ushort.MaxValue}");
+            Console.WriteLine($"Min value of ushort {ushort.MinValue}\n");
+
+            Console.WriteLine($"Size of int {sizeof(int)}");
+            Console.WriteLine($"Max value of int {int.MaxValue}");
+            Console.WriteLine($"Min value of int {int.MinValue}\n");
+
+            Console.WriteLine($"Size of uint {sizeof(uint)}");
+            Console.WriteLine($"Max value of uint {uint.MaxValue}");
+            Console.WriteLine($"Min value of uint {uint.MinValue}\n");
+
+            Console.WriteLine($"Size of long {sizeof(long)}");
+            Console.WriteLine($"Max value of long {long.MaxValue}");
+            Console.WriteLine($"Min value of long {long.MinValue}\n");
+
+            Console.WriteLine($"Size of ulong {sizeof(ulong)}");
+            Console.WriteLine($"Max value of ulong {ulong.MaxValue}");
+            Console.WriteLine($"Min value of ulong {ulong.MinValue}\n");
+        }
+    }
+}
