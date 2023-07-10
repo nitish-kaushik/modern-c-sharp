a37779dfac80489b8b945e8e379c556ede9e9eff
Nitish <nitish.webgentle@gmail.com>
2023-07-10 21:09:28 +0530

    Added new files

---
+++ 34/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 10;
+
+            var area = Math.PI * r * r;
+
+            Console.WriteLine(area);
+        }
+    }
+}
